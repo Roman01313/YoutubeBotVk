8d5d6e4f2a44885067ebeb7171a01b4c90f76a2b
Roman01313 <roman_bobrov013@mail.ru>
2024-12-13 15:26:55 +0300

    Version 1

--- VkBot.py
+++ VkBot.py
@@ -1,12 +1,9 @@
-from itertools import count
-
 import vk_api
 from vk_api.longpoll import VkLongPoll, VkEventType
 from random import randrange
 from youtubesearchpython import *
 from dotenv import load_dotenv
 import os
-from TikTokAPI import TikTokAPI
 
 load_dotenv()
 
@@ -15,10 +12,24 @@ vk_session = vk_api.VkApi(token=TOKEN)
 longpoll = VkLongPoll(vk_session)
 vk = vk_session.get_api()
 
+
+
 for event in longpoll.listen():
     if event.type == VkEventType.MESSAGE_NEW and event.text and event.to_me:
         if event.from_user:
             link = VideosSearch(event.text, limit=3)
+            print(link.result())
             for i in range(len(link.result()['result'])):
                 vid = link.result()['result'][i]['link']
-                vk.messages.send(user_id=event.user_id, message=f'Найдено по вашему запросу :{vid}', random_id=randrange(1,10000))
+                tit = link.result()['result'][i]['title']
+                creator = link.result()['result'][i]['channel']['name']
+                publish_time = link.result()['result'][i]['publishedTime']
+                duration = link.result()['result'][i]['duration']
+                views = link.result()['result'][i]['viewCount']['short']
+                vk.messages.send(user_id=event.user_id, message=f'Автор:{creator}\n'
+                                                                f'Название:{tit}\n'
+                                                                f'Найдено по вашему запросу :{vid}\n'
+                                                                f'Дата публикации: {publish_time}\n'
+                                                                f'Длительность: {duration}\n'
+                                                                f'Кол-во просмотров:{views}\n',
+                                 random_id=randrange(1,10000))
