6f19bd43b480de2b7e29c856865ec05586908ed2
Roman01313 <roman_bobrov013@mail.ru>
2024-12-14 16:25:34 +0300

    Bot Ver2

--- VkBot.py
+++ VkBot.py
@@ -1,6 +1,6 @@
 import vk_api
 from vk_api.longpoll import VkLongPoll, VkEventType
-from random import randrange
+from random import randrange, random
 from youtubesearchpython import *
 from dotenv import load_dotenv
 import os
@@ -12,24 +12,41 @@ vk_session = vk_api.VkApi(token=TOKEN)
 longpoll = VkLongPoll(vk_session)
 vk = vk_session.get_api()
 
+#доступные команды
+commands = {
+    '/название' : 'title',
+    '/дата' : 'publishedTime',
+    '/длительность' : 'duration',
+    '/просмотры' : 'viewCount',
+    '/автор' : 'channel',
+    '/инфо' : 'accessibility'
+}
 
+
+def get_result(video_link, data): #однократное исполнение команд
+    #исключения для более развернутых ответов
+    if data == 'viewCount':
+        result = video_link.result()['result'][0][data]['short']
+    elif data == 'channel':
+        result = video_link.result()['result'][0][data]['name']
+    elif data == 'accessibility':
+        result = video_link.result()['result'][0][data]['title']
+    else:
+        #единый способ исполнения команды
+        result = video_link.result()['result'][0][data]
+    return result  #возращение результата после команды
 
 for event in longpoll.listen():
     if event.type == VkEventType.MESSAGE_NEW and event.text and event.to_me:
-        if event.from_user:
-            link = VideosSearch(event.text, limit=3)
-            print(link.result())
-            for i in range(len(link.result()['result'])):
-                vid = link.result()['result'][i]['link']
-                tit = link.result()['result'][i]['title']
-                creator = link.result()['result'][i]['channel']['name']
-                publish_time = link.result()['result'][i]['publishedTime']
-                duration = link.result()['result'][i]['duration']
-                views = link.result()['result'][i]['viewCount']['short']
-                vk.messages.send(user_id=event.user_id, message=f'Автор:{creator}\n'
-                                                                f'Название:{tit}\n'
-                                                                f'Найдено по вашему запросу :{vid}\n'
-                                                                f'Дата публикации: {publish_time}\n'
-                                                                f'Длительность: {duration}\n'
-                                                                f'Кол-во просмотров:{views}\n',
-                                 random_id=randrange(1,10000))
+        if event.from_user and (event.text).lower() not in commands: #поиск видео
+            link = VideosSearch(event.text,limit=1)
+            vk.messages.send(user_id=event.user_id,
+                             message=get_result(video_link=link, data='link'),
+                             random_id = randrange(0,10000))
+                            #результат поиска
+        elif event.from_user and (event.text).lower() in commands: #контекст исполнение команд
+            vk.messages.send(user_id=event.user_id,
+                             message=get_result(
+                                 video_link=link,
+                                 data=commands[(event.text).lower()]),
+                             random_id=randrange(0,10000))
